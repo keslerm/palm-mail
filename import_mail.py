23574e19b383991a866f988f4ffce9cb71d30671
rridley <rridley@port53.me>
2022-10-14 13:10:27 -0400

    more work as well as dealing with special characters

--- import_mail.py
+++ import_mail.py
@@ -2,6 +2,7 @@ from imapclient import IMAPClient
 import os, sys, argparse, subprocess
 import sqlite3
 import email.parser
+from email import policy
 
 db = sqlite3.connect("email.db")
 
@@ -19,7 +20,7 @@ def addressString(entry):
 
 def setupDatabase():
     table = """CREATE TABLE emails (
-    id int,
+    id int primary key,
     from_email varchar,
     to_email varchar,
     subject varchar,
@@ -36,6 +37,12 @@ def setupDatabase():
     print("Database is setup")
 
 
+def clearDatabase():
+    cur = db.cursor()
+    cur.execute("delete from emails")
+    db.commit()
+
+
 def fetchEmail():
     # context manager ensures the session is cleaned up
     with IMAPClient(host=os.environ["EMAIL_HOST"]) as client:
@@ -44,7 +51,7 @@ def fetchEmail():
 
         # search criteria are passed in a straightforward way
         # (nesting is supported)
-        messages = client.search(["ALL"])
+        messages = client.search(["ALL"], "UTF-8")
 
         # fetch selectors are passed as a simple list of strings.
         response = client.fetch(
@@ -59,6 +66,7 @@ def fetchEmail():
             envelope = data[b"ENVELOPE"]
             read = b"\\Seen" in data[b"FLAGS"]
 
+            """
             if os.environ.get("EMAIL_DEBUG"):
                 print("=====START")
                 # print(data[b'BODY[TEXT]'])
@@ -70,31 +78,63 @@ def fetchEmail():
                 print(data[b"FLAGS"])
                 print("read? %s" % (read))
                 print("=====END")
-
+            """
 
             # Some email parsing because email is amazingly crazy
             body = ""
-            fullEmail = email.message_from_bytes(data[b'BODY[]'])
+            fullEmail = email.message_from_bytes(data[b"BODY[]"], policy=policy.default)
+            print(fullEmail.get("From"))
             for part in fullEmail.walk():
+                print(part.get_content_type())
                 if part.get_content_type() == "text/plain":
-                    body = part.get_payload()
+                    body = cleanBody(part.get_payload(decode=True))
+
+            message = {
+                "id": message_id,
+                "from": clean(fullEmail.get("From")),
+                "to": clean(fullEmail.get("To")),
+                "subject": clean(fullEmail.get("Subject")),
+                "timestamp": envelope.date,
+                "is_read": read,
+                "body": body,
+            }
+
+            messages.append(message)
 
             if os.environ.get("EMAIL_DEBUG"):
+                print("====================START")
+                print(f"ID: {message['id']}")
+                print(f"FROM: {message['from']}")
+                print(f"TO: {message['to']}")
+                print(f"SUBJECT: {message['subject']}")
+                print(f"TIMESTAMP: {message['timestamp']}")
+                print(f"IS_READ: {message['is_read']}")
+                print("BODY:")
                 print(body)
-
-            messages.append(
-                {
-                    "id": message_id,
-                    "from": addressString(envelope.from_[0]),
-                    "to": addressString(envelope.to[0]),
-                    "subject": envelope.subject.decode(),
-                    "timestamp": envelope.date,
-                    "is_read": read,
-                    "body": body,
-                }
-            )
+                print("====================END")
 
         return messages
+
+
+def cleanBody(body):
+    """
+    Attempt to strip every bit of unicode out as well as fixing the line endings since Palm
+    expects unix endings but the library gives me windows endings.
+    """
+
+    # replacement strings
+    WINDOWS_LINE_ENDING = "\r\n"
+    UNIX_LINE_ENDING = "\n"
+
+    result = body.decode("utf-8", "ignore").replace(
+        WINDOWS_LINE_ENDING, UNIX_LINE_ENDING
+    )
+    return result
+
+
+def clean(string):
+
+    return string.encode("ascii", "ignore").decode()
 
 
 def sync():
@@ -103,6 +143,9 @@ def sync():
         os.remove("MailDB.pdb")
     except:
         None
+
+    # remove entries for now
+    clearDatabase()
 
     # import new emails
     print("Fetching emails")
@@ -131,6 +174,7 @@ def sync():
     # Generate the new PDB file
     print("Generating PDB file")
     subprocess.run(["perl", "generate_pdb.pl"])
+
 
 def main(argv):
     # Initialize parser
